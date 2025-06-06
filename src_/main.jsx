abf5dae27d71c53e963d9b37f8c51615060d713d
prodigi231 <fr8236787@gmail.com>
2025-06-06 03:07:22 -0600

    Update main.jsx

--- src_/main.jsx
+++ src_/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 function App() {
-  return <h1>Hola, DocuAsesor</h1>
+  return <h1>Hola desde DocuAsesor</h1>
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
